0357880b8f096b6dcc38fd6d183b0943e3cad365
joske <joskekostic@gmail.com>
2019-12-05 19:57:35 +0100

    -command for publishing news

---
+++ app/Console/Commands/PublishNews.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use App\Repositories\News\NewsRepository;
+use App\Support\Enum\NewsStatus;
+
+class PublishNews extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'news:publish';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Publish all scheduled news';
+
+    /** @var NewsRepository */
+    private $news;
+
+    /**
+     * Create a new command instance.
+     *
+     * @param
+     * @return void
+     */
+    public function __construct(NewsRepository $news)
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $news = $this->news->all();
+
+        foreach ($news as $newsItem) {
+            if(Carbon::parse($newsItem->published_at)->isPast()) {
+                $this->news->update($newsItem->id, ["status" => NewsStatus::PUB]);
+            }
+        }
+
+        //TODO: send mail to user
+    }
+}
